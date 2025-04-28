86e3fe497eef5efeddb501d0e9853e968c0a449c
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2025-04-28 15:24:19 +0600

    Restructure .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,7 +1,14 @@
 import os
 
 def Settings(**kwargs):
-    return{
-            'errorformat': '%f:%l:%c:%m',
-            'flags':['-x', 'c++', '-std=c++20', '-g', '-Wall', '-Wextra', '-Wpedantic', '-Werror', '-Wno-unused-parameter', '-Wno-unused-private-field', '-Wno-unused-variable', '-Iinclude/', '-Ivendor/include', '-I/usr/include/bullet'],
-            }
+    ret_dict = {
+        'errorformat': '%f:%l:%c:%m',
+        'flags':
+        [
+            '-x', 'c++', '-std=c++20', '-g', '-Wall', '-Wextra', '-Wpedantic', '-Werror',
+            '-Wno-unused-parameter', '-Wno-unused-private-field', '-Wno-unused-variable',
+            '-Iinclude/', '-Ivendor/include', '-I/usr/include/bullet'
+        ]
+    }
+    return ret_dict
+
